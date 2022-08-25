763e4d6fdd12b119aaca99c6428a8c1135d1602f
XiaoJie <qq1274390921@gmail.com>
2022-08-25 11:39:28 +0800

    升级JDK版本解决Windows缩放问题

--- src/开始游戏.c
+++ src/开始游戏.c
@@ -1,36 +1,22 @@
-ï»¿// å¼€å§‹æ¸¸æˆ.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <stdio.h>
 #include <string.h>
 
-
 int main()
 {
+    printf("Çë°²×°Java / JDK 11¼°ÒÔÉÏ°æ±¾\n");
     char* commend_start_game = "java starter.GameStart";
     char* commend_check_java_exist = "java --version";
     int i = system(commend_check_java_exist);
-    if (i !=0 ) 
+    if (i !=0 )
     {
         system("cls");
         printf("=========================================\n");
-        printf("æœªå®‰è£…Javaæˆ–æœªè®¾ç½®ç¯å¢ƒå˜é‡\n");
+        printf("Î´°²×°Java11»òÎ´ÉèÖÃ»·¾³±äÁ¿\n");
         printf("=========================================\n");
+        system("pause");
         return 1;
-  
     }
     system(commend_start_game);
 
-    
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
